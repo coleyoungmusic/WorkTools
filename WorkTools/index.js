e059927758212b4544e56f30ef744d0986461d41
Micheal Cole Young <coleyoungmusic@gmail.com>
2018-08-06 20:44:22 -0500

    Update index.js

--- WorkTools/index.js
+++ WorkTools/index.js
@@ -1,1 +1,13 @@
+function lowerCase(id) {
+  // Use the .toLowerCase() call
+  var string = document.getElementById(id).value;
+  var converted = document.getElementById(id).value = string.toLowerCase();
+}
 
+function titleCase(id) {
+  var string = document.getElementById(id).value.toLowerCase().split(' ');
+  for (var i = 0; i < string.length; i++) {
+    string[i] = string[i].charAt(0).toUpperCase() + string[i].slice(1);
+    var converted = document.getElementById(id).value = string.join(' ');
+  }
+}
